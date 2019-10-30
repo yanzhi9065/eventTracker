7fc3d9d0db55e624b9ce6059772db395852fa68a
Yanzhi <yanzhi@formatech.com>
2019-10-29 18:59:35 -0700

    use redis to cnt

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express')
+const redis = require('redis')
 const app = express()
 const port = 8088
+
+redisClient    = redis.createClient({
+  port      : 6379,               // replace with your port
+  host      : '127.0.0.1',        // replace with your hostanme or IP address
+  // optional, if using SSL
+  // use `fs.readFile[Sync]` or another method to bring these values in
+});
 
 app.get('/', (req, res) => res.send('hello world'))
 
@@ -8,11 +16,10 @@ app.get('/logo', (req, res) => {
   console.log("receive a call")
 	let query = req.query
 	if(query) {
-		let deviceId = req.query.deviceId
-		console.log("query", req.query)
-		console.log("get a device:", deviceId)
-		if(req.query.userTag)
-			console.log("userTAG:", req.query.userTag)
+      let deviceId = req.query.deviceId
+      console.log("get a device:", deviceId)
+      if(deviceId)
+        redisClient.incr(deviceId)
 	}
 		
 	res.send("")
