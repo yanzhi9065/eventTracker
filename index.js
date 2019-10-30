f2093f2e0cf2abe25ea02208d7d8724cf4323c16
Yanzhi <yanzhi@formatech.com>
2019-10-29 20:40:39 -0700

    add new api to display result

--- index.js
+++ index.js
@@ -18,11 +18,57 @@ app.get('/logo', (req, res) => {
 	if(query) {
       let deviceId = req.query.deviceId
       console.log("get a device:", deviceId)
-      if(deviceId)
+      if(deviceId) {
         redisClient.incr(deviceId)
+      }
 	}
-		
+    redisClient.incr("total")
 	res.send("")
+})
+
+getRedis = (key) => {
+  return new Promise((resolve,reject)=> {
+    redisClient.get(key, (err, value)=> {
+      if(err){
+        reject(err)
+      }
+      resolve(value)
+    })
+  })
+}
+
+packKeys = (keys) => {
+  return new Promise(async (resolve, reject) => {
+    try{
+      body = {}
+      body.result = {}
+      body.size = keys.length
+      for(let i=0; i< keys.length; i++) {
+        let key = keys[i]
+          let cnt = await getRedis(key)
+          console.log("one record:", key, cnt)
+          body.result[key] = cnt
+      }
+      resolve(body)
+    } catch(err) {
+      console.log("reject:", err)
+      reject(err)
+    }
+  });
+}
+
+app.get('/allKeys', (req, res) => {
+  console.log("try to get all keys")
+  redisClient.keys('*', (err , keys) => {
+    if(err) res.status(500).send(err)
+    console.log("keys:",keys)
+    packKeys(keys).then(body => {
+      console.log("send res")
+      res.send(body) 
+    }).catch(err => {
+      res.status(500).send(err)
+    })
+  })
 })
 
 app.listen(port, () => console.log("example is running"))
